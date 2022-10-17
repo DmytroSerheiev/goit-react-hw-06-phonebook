6eb4ff51d9075bee116e33e021dd3a0e8e0b66c5
Dmytro <DSerSmart@gmail.com>
2022-10-17 20:18:47 +0300

    finish

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Layout from './Layout/Layout';
 import Section from './Section/Section';
 import FormPhone from './FormPhone/FormPhone';
@@ -8,82 +8,73 @@ import { v4 as uuidv4 } from 'uuid';
 
 import './App.css';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parseContacts = JSON.parse(contacts);
-    if (parseContacts)
-      this.setState({
-        contacts: parseContacts,
-      });
-  }
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  componentDidUpdate(prevState) {
-      if (this.state.contacts !== prevState.contacts) {
-    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-      }
-  }
+  //===================================================
+  //  componentDidMount() {
+  //     const contacts = localStorage.getItem('contacts');
+  //     const parseContacts = JSON.parse(contacts);
+  //     if (parseContacts)
+  //       this.setState({
+  //         contacts: parseContacts,
+  //       });
+  //   }
+  //   componentDidUpdate() {
+  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  //   }
+  //=======================================================
 
-  addContactPhone = ({ name, number }) => {
-    if (this.state.contacts.find(contact => contact.name === name)) {
+  const addContactPhone = (name, number) => {
+    if (contacts.find(contact => contact.name === name)) {
       alert(`${name} уже есть в списке ваших контактов`);
       return;
     }
+
     const contact = {
       id: uuidv4(),
       name,
       number,
     };
-    this.setState(state => {
-      return { contacts: [...state.contacts, contact] };
-    });
+
+    setContacts(prevState => [...prevState, contact]);
   };
 
-  deleteContact = contactID => {
-    this.setState(state => {
-      return {
-        contacts: state.contacts.filter(({ id }) => id !== contactID),
-      };
-    });
+  const deleteContact = contactID => {
+    setContacts(contacts.filter(({ id }) => id !== contactID));
   };
 
-  changeFilter = filter => {
-    this.setState({ filter });
+  const changeFilter = filter => {
+    setFilter(filter);
   };
 
-  getVisibleContacts = () => {
-    const { contacts, filter } = this.state;
+  const getVisibleContacts = () => {
     return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
+      contact.name.toLowerCase().includes(filter)
     );
   };
 
-  render() {
-    const { contacts, filter } = this.state;
-    const visibleContacts = this.getVisibleContacts();
-    return (
-      <Layout>
-        <h1>Home Work #2.2</h1>
-        <Section title="Phonebook">
-          <FormPhone addContactPhone={this.addContactPhone} />
+  const visibleContacts = getVisibleContacts();
+
+  return (
+    <Layout>
+      <h1>Home Work #4</h1>
+      <Section title="Phonebook">
+        <FormPhone addContactPhone={addContactPhone} />
+      </Section>
+      {!!contacts.length && (
+        <Section title="Contacts">
+          <ContactList
+            contacts={visibleContacts}
+            filter={filter}
+            onChangeFilter={changeFilter}
+            onDeleteContact={deleteContact}
+          />
         </Section>
-        {!!contacts.length && (
-          <Section title="Contacts">
-            <ContactList
-              contacts={visibleContacts}
-              filter={filter}
-              onChangeFilter={this.changeFilter}
-              onDeleteContact={this.deleteContact}
-            />
-          </Section>
-        )}
-      </Layout>
-    );
-  }
-}
+      )}
+    </Layout>
+  );
+};
 
 export default App;
--- src/components/FormPhone/FormPhone.js
+++ src/components/FormPhone/FormPhone.js
@@ -1,63 +1,70 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import s from './FormPhone.module.css';
 
-class FormPhone extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
-  //валидация пустого поля до 16 строчки
-  handleSubmit = e => {
+const FormPhone = ({ addContactPhone }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleSubmit = e => {
     e.preventDefault();
-    console.log(!this.state.name, !this.state.number);
-    if (!this.state.name || !this.state.number) {
+
+    if (!name || !number) {
       alert('Вы не ввели все контактные данные');
       return;
     }
-    //валидация цифры
-    if (Number.isNaN(+this.state.number)) {
+
+    if (Number.isNaN(+number)) {
       alert('Телефонный номер должен содержать только цифры');
       return;
     }
-    this.props.addContactPhone(this.state);
-    this.setState({
-      name: '',
-      number: '',
-    });
+
+    addContactPhone(name, number);
+    setName('');
+    setNumber('');
   };
 
-  handleInputChange = e => {
+  const handleInputChange = e => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
+
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+
+      case 'number':
+        setNumber(value);
+        break;
+
+      default:
+        return;
+    }
+
+    // this.setState({ [name]: value });
   };
 
-  render() {
-    const { name, number } = this.state;
-
-    return (
-      <form className={s.form} onSubmit={this.handleSubmit}>
-        <label>
-          Name
-          <input
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <label>
-          Number
-          <input
-            name="number"
-            type="tel"
-            value={number}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <button type="submit">Add contact</button>
-      </form>
-    );
-  }
-}
+  return (
+    <form className={s.form} onSubmit={handleSubmit}>
+      <label>
+        Name
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleInputChange}
+        />
+      </label>
+      <label>
+        Number
+        <input
+          name="number"
+          type="tel"
+          value={number}
+          onChange={handleInputChange}
+        />
+      </label>
+      <button type="submit">Add contact</button>
+    </form>
+  );
+};
 
 export default FormPhone;
